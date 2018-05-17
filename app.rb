934725c564fc1c19dfe84e82e5366f4f4931d97e
Pat Cody <patcody028@gmail.com>
2018-05-17 13:43:34 -0400

    Removed unnecessary code

--- app.rb
+++ app.rb
@@ -5,16 +5,12 @@ get '/' do
   @cake_list = File.open("cake.list", "r")
   @counter = 1
   erb :index
-  #@cake_list.close
 end
 
 get '/json' do
-  #@cake_list = File.open("cake.list", "r")
   @json_array = File.readlines("cake.list")
   @json_array.map!{ |cake| cake.gsub(/\n/, '') }
-  @json_array.to_json
-  #@test_array = [0, 1, 2]
-  #@test_array.to_json
+  json @json_array
 end
 
 get '/test' do
