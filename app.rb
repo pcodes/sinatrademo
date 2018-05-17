1161e49c36095bbfe9a51c2ea2109e724eaffd90
Pat Cody <patcody028@gmail.com>
2018-05-17 13:35:35 -0400

    Added JSON response and updated gitignore

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
 require 'sinatra'
-require 'rubygems'
+require 'sinatra/json'
 
 get '/' do
   @cake_list = File.open("cake.list", "r")
   @counter = 1
   erb :index
   #@cake_list.close
+end
+
+get '/json' do
+  #@cake_list = File.open("cake.list", "r")
+  @json_array = File.readlines("cake.list")
+  @json_array.map!{ |cake| cake.gsub(/\n/, '') }
+  @json_array.to_json
+  #@test_array = [0, 1, 2]
+  #@test_array.to_json
 end
 
 get '/test' do
